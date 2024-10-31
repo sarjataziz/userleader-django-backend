3940df465b38a2a7557b0d2855ec00a6e81a3bf8
sarjaaziz <sarjat.aziz@gmail.com>
2024-10-31 13:42:14 +0600

    Fixed and update the download.py file

--- download_model.py
+++ download_model.py
@@ -1,13 +1,24 @@
-import gdown
+import requests
 import os
 
-file_id = '1jFTayEKAzTltJkMeTNXgJcXi9-pbX4cZ' 
-url = f"https://drive.google.com/uc?id={file_id}&confirm=t"
+url = "https://huggingface.co/sarjat/best_rf_model/resolve/main/best_rf_model.pkl"  
 output_path = os.path.join(os.path.dirname(__file__), 'userleader_app', 'models', 'best_rf_model.pkl')
 
+def download_file(url, output_path):
+    try:
+        print(f"Downloading model from {url}...")
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            with open(output_path, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+        print("Download complete!")
+    except Exception as e:
+        print(f"Failed to download the file: {e}")
+        exit(1)
+
 if not os.path.exists(output_path):
-    print("Downloading model file using gdown...")
-    gdown.download(url, output_path, quiet=False)
-    print("Download complete!")
+    download_file(url, output_path)
 else:
     print("Model file already exists.")
