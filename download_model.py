70f53ad94ccb775a93eb1202eb2adef3f1d6dd7e
sarjaaziz <sarjat.aziz@gmail.com>
2024-11-03 11:31:06 +0600

    Update model download script and .gitignore

--- download_model.py
+++ download_model.py
@@ -1,18 +1,17 @@
 import requests
 import os
 
-url = "https://huggingface.co/sarjat/best_rf_model/resolve/main/best_rf_model.pkl"  
+url = "https://huggingface.co/sarjat/best_rf_model/resolve/main/best_rf_model.pkl"
 output_path = os.path.join(os.path.dirname(__file__), 'userleader_app', 'models', 'best_rf_model.pkl')
 
 def download_file(url, output_path):
     try:
         print(f"Downloading model from {url}...")
-        with requests.get(url, stream=True) as response:
-            response.raise_for_status()
-            with open(output_path, 'wb') as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:
-                        f.write(chunk)
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        with open(output_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
         print("Download complete!")
     except Exception as e:
         print(f"Failed to download the file: {e}")
@@ -22,4 +21,3 @@ if not os.path.exists(output_path):
     download_file(url, output_path)
 else:
     print("Model file already exists.")
-    
