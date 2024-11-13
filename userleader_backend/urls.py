ef8f86558745b0c73a0d352a15685ca8bef1ce04
sarjaaziz <sarjat.aziz@gmail.com>
2024-11-13 13:33:13 +0600

    Update project files, fix internal server error, update requirements

--- userleader_backend/urls.py
+++ userleader_backend/urls.py
@@ -37,6 +37,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', homepage),  # Root URL
+    path('api/v1/', include('userleader_app.urls')),
     path('docs<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
